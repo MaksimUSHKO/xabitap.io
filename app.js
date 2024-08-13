663fb2e8f3c23071888665ac4efb8b7048bf04e3
MaksimUSHKO <64306170+MaksimUSHKO@users.noreply.github.com>
2024-08-13 18:48:27 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let_item = "";
 fetch("user_info.json", {cache: 'no-store'})
   .then(response => response.json())
   .then(json => console.log(json));
-  alert(json[0].score);
+  alert(json[0]);
   let number = json[0].score;
 
 
